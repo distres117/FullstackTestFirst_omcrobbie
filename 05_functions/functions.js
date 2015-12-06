0803ef1ceb9fb790e53fa649d96e9b0741851cf2
distres117 <omcrobbie@gmail.com>
2015-12-06 18:18:21 -0500

    Ex 5 complete

--- 05_functions/functions.js
+++ 05_functions/functions.js
@@ -1,1 +1,41 @@
 // Functions.js
+function concat_string(){
+  var args = Array.prototype.slice.call(arguments);
+  return args.filter(i=>i!==" ").join(" ");
+}
+
+function yourFunctionRunner(){
+  var rtn = "";
+  for(var i in arguments)
+    rtn+=arguments[i].call();
+  return rtn;
+}
+
+function makeAdder(num){
+  return function(newNum){
+    return newNum + num;
+  }
+}
+
+function once(func){
+  var runs = 0;
+  return function(){
+    runs++;
+    if (runs < 2) func();
+  }
+}
+
+function createObjectWithTwoClosures(){
+  var val = 0;
+  return {
+    oneIncrementer: function(){
+      val+=1;
+    },
+    tensIncrementer: function(){
+      val+=10;
+    },
+    getValue: function(){
+      return val;
+    }
+  }
+}
